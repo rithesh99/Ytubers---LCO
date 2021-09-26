8ef171b15a9ae3a40b8f3b7cdee3d8953d2b20e0
rithesh99 <ritheshdavid@gmail.com>
2021-09-26 12:36:48 +0530

    Team section

--- tubers/webpages/models.py
+++ tubers/webpages/models.py
@@ -11,3 +11,16 @@ class Slider(models.Model):
 
     def __str__(self):
         return self.headline
+
+class Team(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    role = models.CharField(max_length=100)
+    yt_link = models.CharField(max_length=255)
+    fb_link = models.CharField(max_length=255)
+    insta_link = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='media/team/%Y/%m/%d')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
