40285659775efb46e758782eb39e9997156c0970
rithesh99 <ritheshdavid@gmail.com>
2021-09-27 23:33:42 +0530

    Search works

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -11,11 +11,41 @@ def youtubers(request):
 
 def youtubers_detail(request, id):
     ytuber = get_object_or_404(Youtuber, pk=id)
-    print(ytuber)
     data = {
         'ytuber' : ytuber
     }
     return render(request, 'youtubers/youtuber.html', data)
 
 def search(request):
-    pass
+    ytubers = Youtuber.objects.order_by('-created_at')
+    city_options = Youtuber.objects.values_list('city', flat=True).distinct()
+    camera_options = Youtuber.objects.values_list('camera_type', flat=True).distinct()
+    category_options = Youtuber.objects.values_list('category', flat=True).distinct()
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            ytubers = ytubers.filter(description__icontains=keyword)
+    
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            ytubers = ytubers.filter(city__icontains=city)
+    
+    if 'camera' in request.GET:
+        camera = request.GET['camera']
+        if camera:
+            ytubers = ytubers.filter(camera_type__icontains=camera)
+    
+    if 'category' in request.GET:
+        category = request.GET['category']
+        if category:
+            ytubers = ytubers.filter(category__icontains=category)
+    
+    data = {
+        'ytubers' : ytubers,
+        'city_options' : city_options,
+        'camera_options' : camera_options,
+        'category_options' : category_options
+    }
+    return render(request, 'youtubers/search.html', data)
